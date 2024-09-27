8084e33be71e1fbd5f524a1d1b79ea9222826ac6
Aczélos Csaba <csaba.aczelos@gmail.com>
2024-09-27 13:10:11 +0200

    Experimenting with login

--- backend_app/core/views.py
+++ backend_app/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
+class Login(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        print("request received")
+        serializer = UserSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
