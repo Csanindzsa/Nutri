65d7902378a1f8a4704bc3040137b3beed9a226c
Aczélos Csaba <csaba.aczelos@gmail.com>
2024-09-30 19:54:37 +0200

    Basic account handling is supported

--- backend_app/core/urls.py
+++ backend_app/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Login
+from .views import Register, Account
 
 urlpatterns = [
-    path("user", Login.as_view()),
+    # path("user", Login.as_view()),
+    path("register", Register.as_view()),
+    path("account/<int:pk>", Account.as_view()),
 ]
