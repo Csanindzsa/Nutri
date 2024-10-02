b910b78835cccab42691fb49fe2b7d981132f362
Aczélos Csaba <csaba.aczelos@gmail.com>
2024-10-02 18:27:02 +0200

    second backend project works, migrations were succesfull

---
+++ backend_second/backend_second/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("", include("nutrition_section.urls")),
+    path('rest/', include('rest_framework.urls')),
+
+    ##these are for the user's login
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+]
