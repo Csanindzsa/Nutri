b903ac8071c983b2ed6a1dd0f2346c0fb16510c1
Aczélos Csaba <csaba.aczelos@gmail.com>
2024-11-13 18:08:56 +0100

    cleanup

--- backend_second/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import Register, Account
-
-urlpatterns = [
-    # path("user", Login.as_view()),
-    path("register", Register.as_view()),
-    path("account/<int:pk>", Account.as_view()),
-]
--- backend_second/nutrition_section/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ingredient, Product
-
-# Register your models here.
-admin.site.register(Ingredient)
-admin.site.register(Product)
